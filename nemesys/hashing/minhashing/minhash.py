39e4427c1e5735437789eb018f9efbaa6fe9a420
Miljenko <headsouldev@gmail.com>
2021-11-06 23:42:12 +0100

    Add hashing

---
+++ nemesys/hashing/minhashing/minhash.py
@@ -0,0 +1,161 @@
+from typing import Any, Callable, Iterable, Optional
+
+
+class MinHash:
+    def __init__(
+        self,
+        n_permutations: Any,
+        seed: Any,
+        preprocess_function: Callable[[Any], Any],
+        prime: Optional[Any] = None,
+        base_state: Optional[Any] = None,
+        bound: Optional[Any] = None,
+    ):
+        self._n_permutations = n_permutations
+        self._seed = seed
+        self._preprocess_function = preprocess_function
+
+        self._prime = prime
+        self._base_state = base_state
+        self._state = None
+        self._bound = bound
+
+        self.clear()
+
+    # region Properties
+    @property
+    def a(self) -> Any:
+        raise NotImplementedError
+
+    @property
+    def b(self) -> Any:
+        raise NotImplementedError
+
+    @property
+    def base_state(self) -> Any:
+        return self._base_state
+
+    @property
+    def bound(self) -> Any:
+        return self._bound
+
+    @property
+    def n_permutations(self) -> Any:
+        return self._n_permutations
+
+    @property
+    def preprocess_function(self) -> Callable[[Any], Any]:
+        return self._preprocess_function
+
+    @property
+    def prime(self) -> Any:
+        return self._prime
+
+    @property
+    def seed(self) -> Any:
+        return self._seed
+
+    @property
+    def state(self) -> Any:
+        return self._state
+
+    @state.getter
+    def state(self, value: Any):
+        self._state = value
+
+    # endregion
+
+    # region Single implementation
+    @staticmethod
+    def preprocess(data: Any, preprocess_function: Callable[[Any], Any]) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def data_to_base_hash(data: Any, a: Any, b: Any, prime: Any) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def base_hash_to_bounded_hash(base_hash: Any, bound: Any) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def bounded_hash_to_minhash(bounded_hash: Any, state: Any) -> Any:
+        raise NotImplementedError
+
+    # endregion
+
+    # region Batch implementation
+    @staticmethod
+    def preprocess_batch(
+        data_batch: Iterable[Any], preprocess_function: Callable[[Any], Any]
+    ) -> Iterable[Any]:
+        raise NotImplementedError
+
+    @staticmethod
+    def data_batch_to_base_hashes(
+        data_batch: Iterable[Any], a: Any, b: Any, prime: Any
+    ) -> Iterable[Any]:
+        raise NotImplementedError
+
+    @staticmethod
+    def base_hashes_to_bounded_hashes(
+        base_hashes: Iterable[Any], bound: Any
+    ) -> Iterable[Any]:
+        raise NotImplementedError
+
+    @staticmethod
+    def bounded_hashes_to_minhash(bounded_hashes: Iterable[Any], state: Any) -> Any:
+        raise NotImplementedError
+
+    # endregion
+
+    # region Single hashing
+    def get_hash_eager(self, data: Any):
+        preprocessed = self.preprocess(
+            data=data, preprocess_function=self.preprocess_function
+        )
+        base_hash = self.data_to_base_hash(
+            data=preprocessed, a=self.a, b=self.b, prime=self.prime
+        )
+        bounded_hash = self.base_hash_to_bounded_hash(
+            base_hash=base_hash, bound=self.bound
+        )
+
+        return bounded_hash
+
+    def get_minhash_eager(self, data: Any, state: Any):
+        bounded_hash = self.get_hash_eager(data=data)
+        minhash = self.bounded_hash_to_minhash(
+            bounded_hash=bounded_hash, hash_state=state
+        )
+
+        return minhash
+
+    def update(self, data: Any) -> Any:
+        self.state = self.get_minhash_eager(data=data, state=self.state)
+
+    # endregion
+
+    # region Batch hashing
+    def get_hash_batch_eager(self, data_batch: Iterable[Any]):
+        for data in data_batch:
+            yield self.get_hash_eager(data=data)
+
+    def get_minhash_batch_eager(self, data_batch: Iterable[Any], state: Any):
+        for data in data_batch:
+            state = self.get_minhash_eager(data=data, state=state)
+
+        return state
+
+    def update_batch(self, data_batch: Iterable[Any]) -> Any:
+        self.state = self.get_minhash_batch_eager(
+            data_batch=data_batch, state=self.state
+        )
+
+    # endregion
+
+    def clear(self):
+        self.state = self.base_state
+
+    def digest(self) -> Any:
+        return self.state
---
+++ nemesys/hashing/minhashing/numpy_minhash.py
@@ -0,0 +1,172 @@
+from typing import Any, Callable, Iterable, Optional, Union
+
+import numpy as np
+import numpy.typing as npt
+
+from nemesys.hashing.minhashing.minhash import MinHash
+from nemesys.utils.conversions.numpy_conversion import NumPyConversion
+
+
+class NumPyMinHash(MinHash):
+    def __init__(
+        self,
+        n_permutations: int,
+        seed: int,
+        preprocess_function: Callable[[Any], npt.NDArray[np.uint64]],
+        prime: Optional[Union[np.uint64, int]] = None,
+        base_state: Optional[Iterable[Union[np.uint32, int]]] = None,
+        bound: Optional[Union[np.uint64, int]] = None,
+    ):
+        if prime is None:
+            prime = self.default_prime
+        if base_state is None:
+            base_state = self.default_base_state
+        if bound is None:
+            bound = self.bound
+
+        super().__init__(
+            n_permutations=n_permutations,
+            seed=seed,
+            preprocess_function=preprocess_function,
+            prime=NumPyConversion.to_array(prime, dtype=np.uint64),
+            base_state=NumPyConversion.to_array(base_state, dtype=np.uint32),
+            bound=NumPyConversion.to_array(content=bound, dtype=np.uint64),
+        )
+
+        generator = np.random.RandomState(seed=self._seed)
+        self._a = generator.randint(
+            low=1,
+            high=self.prime,
+            size=self.n_permutations,
+            dtype=np.uint64,
+        )
+        self._b = generator.randint(
+            low=0,
+            high=self.prime,
+            size=self.n_permutations,
+            dtype=np.uint64,
+        )
+
+    # region Defaults
+    @property
+    def default_base_state(self) -> npt.NDArray[np.uint64]:
+        return np.array([(2 ** 32) - 1] * self.n_permutations, dtype=np.uint64)
+
+    @property
+    def default_bound(self) -> np.uint64:
+        return np.array((2 ** 32) - 1, dtype=np.uint64)
+
+    @property
+    def default_prime(self) -> np.uint64:
+        return np.array((2 ** 61) - 1, dtype=np.uint64)
+
+    # endregion
+
+    # region Properties
+    @property
+    def a(self) -> npt.NDArray[np.uint64]:
+        return self._a
+
+    @property
+    def b(self) -> npt.NDArray[np.uint64]:
+        return self._b
+
+    @property
+    def base_state(self) -> npt.NDArray[np.uint64]:
+        return np.copy(self._base_state)
+
+    @property
+    def bound(self) -> npt.NDArray[np.uint64]:
+        return self._bound
+
+    @property
+    def n_permutations(self) -> int:
+        return self._n_permutations
+
+    @property
+    def preprocess_function(self) -> Callable[[Any], npt.NDArray[np.uint64]]:
+        return self._preprocessing_function
+
+    @property
+    def prime(self) -> np.uint64:
+        return self._prime
+
+    @property
+    def seed(self) -> int:
+        return self._seed
+
+    @property
+    def state(self) -> npt.NDArray[np.uint64]:
+        return np.copy(self._state)
+
+    # endregion
+
+    # region Single implementation
+    @staticmethod
+    def preprocess(
+        data: Any, preprocess_function: Callable[[Any], npt.NDArray[np.uint64]]
+    ) -> Any:
+        return preprocess_function(data)
+
+    @staticmethod
+    def data_to_base_hash(
+        data: npt.NDArray[np.uint64],
+        a: npt.NDArray[np.uint64],
+        b: npt.NDArray[np.uint64],
+        prime: Any,
+    ) -> np.uint64:
+        return ((data * a) + b) % prime
+
+    @staticmethod
+    def base_hash_to_bounded_hash(
+        base_hash: npt.NDArray[np.uint64], bound: np.uint64
+    ) -> npt.NDArray[np.uint64]:
+        return np.bitwise_and(base_hash, bound)
+
+    @staticmethod
+    def bounded_hash_to_minhash(
+        bounded_hash: npt.NDArray[np.uint64], state: npt.NDArray[np.uint64]
+    ) -> npt.NDArray[np.uint64]:
+        return np.minimum(bounded_hash, state)
+
+    # endregion
+
+    # region Batch implementation
+    @staticmethod
+    def preprocess_batch(
+        data_batch: Iterable[Any], preprocess_function: Callable[[Any], Any]
+    ) -> Iterable[Any]:
+        for data in data_batch:
+            yield NumPyMinHash.preprocess(
+                data=data, preprocess_function=preprocess_function
+            )
+
+    @staticmethod
+    def data_batch_to_base_hashes(
+        data_batch: Iterable[npt.NDArray[np.uint64]],
+        a: npt.NDArray[np.uint64],
+        b: npt.NDArray[np.uint64],
+        prime: np.uint64,
+    ) -> Iterable[npt.NDArray[np.uint64]]:
+        for data in data_batch:
+            yield NumPyMinHash.data_to_base_hash(data=data, a=a, b=b, prime=prime)
+
+    @staticmethod
+    def base_hashes_to_bounded_hashes(
+        base_hashes: Iterable[npt.NDArray[np.uint64]], bound: np.uint64
+    ) -> Iterable[npt.NDArray[np.uint64]]:
+        for base_hash in base_hashes:
+            yield NumPyMinHash.base_hash_to_bounded_hash(
+                base_hash=base_hash, bound=bound
+            )
+
+    @staticmethod
+    def bounded_hashes_to_minhash(
+        bounded_hashes: Iterable[npt.NDArray[np.uint64]], state: npt.NDArray[np.uint64]
+    ) -> npt.NDArray[np.uint64]:
+        return np.minimum(
+            np.array(bounded_hashes, dtype=np.uint64),
+            state,
+        )
+
+    # endregion
