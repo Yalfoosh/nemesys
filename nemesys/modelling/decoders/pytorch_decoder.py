77cf94e7761a9d09582e8c82a2ff707a71b3c34b
Miljenko Šuflaj <headsouldev@gmail.com>
2022-01-03 20:14:35 +0100

    [nemesys/modelling/decoders/pytorch_decoder.py] Refactor for multiple inheritance

--- nemesys/modelling/decoders/pytorch_decoder.py
+++ nemesys/modelling/decoders/pytorch_decoder.py
@@ -1,27 +1,25 @@
-from typing import Dict
-
 import torch
 import torch.nn
 
 from nemesys.modelling.decoders.decoder import Decoder
-from nemesys.modelling.stores.store import Store
 
 
-class PyTorchDecoder(Decoder, torch.nn.Module):
-    def __init__(self, module: torch.nn.Module):
-        super().__init__()
+class PyTorchDecoder(torch.nn.Module, Decoder):
+    def __init__(self, module):
+        torch.nn.Module.__init__(self)
+        Decoder.__init__()
 
         self._module = module
 
     # region Properties
     @property
-    def module(self) -> torch.nn.Module:
+    def module(self):
         return self._module
 
     # endregion
 
-    def decode(self, store: Store) -> Dict[str, torch.Tensor]:
+    def decode(self, store):
         return self._module(store)
 
-    def forward(self, store: Store) -> Dict[str, torch.Tensor]:
+    def forward(self, store):
         return self.decode(store=store)
