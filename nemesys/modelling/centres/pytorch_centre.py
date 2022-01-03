b7a03e9fc190294688416ce8e353a5fe4e30e7c3
Miljenko Šuflaj <headsouldev@gmail.com>
2022-01-03 20:03:51 +0100

    [nemesys/modelling/centres/pytorch_centre.py] Refactor for multiple inheritance
    Because a PyTorchCentre is a multiple inheritance object (uses torch.nn.Module
    and nemesys.modelling.centres.Centre), one super init call won't cut it.
    Furthermore, it is necessary to first initialize the torch.nn.Module so it
    can record the Centre components after.
    
    Therefore, we explicitly call the super init methods in the wanted order.

--- nemesys/modelling/centres/pytorch_centre.py
+++ nemesys/modelling/centres/pytorch_centre.py
@@ -1,64 +1,15 @@
-from typing import Dict
-
 import torch
-import torch.nn
 
-from modelling.analysers.pytorch_analyser import PyTorchAnalyser
-from modelling.decoders.pytorch_decoder import PyTorchDecoder
-from modelling.encoders.pytorch_encoder import PyTorchEncoder
-from modelling.routers.pytorch_router import PyTorchRouter
-from modelling.stores.pytorch_store import PyTorchStore
-from modelling.synthesisers.pytorch_synthesiser import PyTorchSynthesiser
+from nemesys.modelling.centres.centre import Centre
 
 
-class PyTorchCentre(torch.nn.Module):
-    def __init__(
-        self,
-        store: PyTorchStore,
-        analyser: PyTorchAnalyser,
-        encoder: PyTorchEncoder,
-        decoder: PyTorchDecoder,
-        router: PyTorchRouter,
-        synthesiser: PyTorchSynthesiser,
-    ):
-        super().__init__()
+class PyTorchCentre(torch.nn.Module, Centre):
+    def __init__(self, analyser, encoder, decoder, router, store, synthesiser):
+        torch.nn.Module.__init__(self)
+        Centre.__init__(self, analyser, decoder, encoder, router, store, synthesiser)
 
-        self._store = store
-        self._analyser = analyser
-        self._encoder = encoder
-        self._decoder = decoder
-        self._router = router
-        self._synthesiser = synthesiser
-
-    # region Properties
-    @property
-    def analyser(self) -> PyTorchAnalyser:
-        return self._analyser
-
-    @property
-    def decoder(self) -> PyTorchDecoder:
-        return self._decoder
-
-    @property
-    def encoder(self) -> PyTorchEncoder:
-        return self._encoder
-
-    @property
-    def router(self) -> PyTorchRouter:
-        return self._router
-
-    @property
-    def store(self) -> PyTorchStore:
-        return self._store
-
-    @property
-    def synthesiser(self) -> PyTorchSynthesiser:
-        return self._synthesiser
-
-    # endregion
-
-    def process(self, inputs: torch.Tensor) -> Dict[str, torch.Tensor]:
+    def process(self, inputs):
         raise NotImplementedError
 
-    def forward(self, inputs: torch.Tensor) -> Dict[str, torch.Tensor]:
+    def forward(self, inputs):
         return self._process(inputs=inputs)
